c57d92afae92dcef93f8362318ce74194973fd52
Dam <damien.labranche@ynov.com>
2019-01-06 23:54:37 +0100

    ajout paddle, reste menu, rebonds sur paddle et conditions victoire à ajouter pour V0.2.

--- main.py
+++ main.py
@@ -15,24 +15,55 @@ class Ball:
     def move(self):
         self.canvas.move(self.id, self.xspeed, self.yspeed)
         pos = self.canvas.coords(self.id)
+        #Manque conditions de rebonds sur les paddles
         if pos[1] <= 0:
             self.yspeed = 3
         if pos[0] <= 0:
             self.xspeed = 3
-        if pos[2] >= 800:
+        if pos[2] >= 400:
             self.xspeed = -3
         if pos[3] >= 600:
             self.yspeed = -3
 
+class Paddle:
+    def __init__(self, canvas, top) :
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0, 100, 10, fill="blue")
+        if(top == "true"):
+            self.canvas.move(self.id, 200,50)
+            self.canvas.bind_all('<KeyPress-q>', self.move_left)
+            self.canvas.bind_all('<KeyPress-d>', self.move_right)
+        else :
+            self.canvas.move(self.id, 200,550)
+            self.canvas.bind_all('<KeyPress-Left>', self.move_left)
+            self.canvas.bind_all('<KeyPress-Right>', self.move_right)
+        self.xspeed = 0
+
+    def draw(self):
+        self.canvas.move(self.id, self.xspeed, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0 :
+            self.xspeed = 0
+        if pos[2] >= 400:
+            self.xspeed = 0
+
+    def move_left(self, evt):
+        self.xspeed = -2
+    def move_right (self, evt):
+        self.xspeed = 2
 
 w = tkinter.Tk()
 w.title("Pong")
-can = tkinter.Canvas (w, bg = "white", height = 600, width = 800)
+can = tkinter.Canvas (w, bg = "white", height = 600, width = 400)
 can.pack()
+paddle_bot = Paddle(can,"false")
+paddle_top = Paddle(can,"true")
 ball = Ball(can)
 
 while 1 :
     ball.move()
+    paddle_bot.draw()
+    paddle_top.draw()
     w.update()
     time.sleep(0.01)
 
